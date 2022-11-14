42e9325942a1984d9fd37c8ba48136bd49789c93
Lev <Lev_tomilov@mail.ru>
2022-11-14 15:28:38 +0500

    add console programm

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-"use strict";
+/* Задание на урок:
 
-if (4 == 9){
-	console.log("Ok!");
-} else {
-	console.log("Error!");
-}
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-// const num = 50;
-// if (num < 49){
-// 	console.log('Error!');
-// } else if (num > 100){
-// 	console.log('Много');
-// } else {
-// 	console.log('Ok!');
-// }
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-// (num == 50) ? console.log('Ok!') : console.log('Error!');
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-const num = 50;
+Проверить, чтобы все работало без ошибок в консоли */
 
-switch (num) {
-	case 49:
-		console.log('Неверно');
-		break;
-	case 100:
-		console.log('Неверно');
-		break;
-	case 50:
-		console.log('В точку!');
-		break;
-	default:
-		console.log('Не в этот раз!');
-		bre
-}
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',' ');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: true,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',' '),
+	  b = prompt('На сколько оцените его?'),
+	  c= prompt('Один из последних просмотренных фильмов?',' '),
+	  d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
